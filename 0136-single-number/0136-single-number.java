2500a99294eef85d801626e60b8f314372b5477f
Babita-Patidar <goal202401@gmail.com>
2026-03-12 23:52:53 +0530

    Time: 11 ms (23.22%), Space: 47.2 MB (12.58%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for(int i = 0; i < n-1; i += 2){
+            if(nums[i] != nums[i+1]){
+                return nums[i];
+            }
+        }
+
+        return nums[n-1];
+    }
+}
