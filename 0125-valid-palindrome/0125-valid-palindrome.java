5be0544914aec3f53acaf3a9f93544f5194b31ca
Babita-Patidar <goal202401@gmail.com>
2026-03-19 20:17:35 +0530

    Time: 3 ms (61.33%), Space: 44.1 MB (90.47%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String str) {
+        str = str.toLowerCase();
+       
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(str.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(str.charAt(right))) {
+                right--;
+            }
+
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
