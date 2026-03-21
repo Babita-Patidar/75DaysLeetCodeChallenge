23529df0021e79f57e132c60b35db94d9a141b9b
Babita-Patidar <goal202401@gmail.com>
2026-03-21 10:15:12 +0530

    Time: 2 ms (90.55%), Space: 48.1 MB (15.13%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,21 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n=nums.length;
-        int zeroes=0;
-        for(int ele:nums){
-            if(ele==0){
-                zeroes++;
+        int j = 0; // pointer for non-zero position
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
             }
         }
-        for(int i=0;i<zeroes;i++){
-            for(int j=0;j<n-1-i;j++){
-                if(nums[j]==0){
-                    int temp=nums[j];
-                    nums[j]=nums[j+1];
-                    nums[j+1]=temp;
-                }
-            }
-        }
-        
     }
 }
